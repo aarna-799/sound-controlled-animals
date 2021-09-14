9609536e13e16feecb8ee7f59994c05f1461c456
aarna-799 <78432502+aarna-799@users.noreply.github.com>
2021-09-14 21:17:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 //https://teachablemachine.withgoogle.com/models/9dafRI3XH/
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/9dafRI3XH/model.json",modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error , results)
+{
